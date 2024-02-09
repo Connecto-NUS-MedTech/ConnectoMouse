4d38580ccd94a758fbcaadf9831655931719d3eb
ilovemcnuggets <136360991+ilovemcnuggets@users.noreply.github.com>
2024-02-09 12:13:00 +0800

    Update nanoesp32blemouse.ino
    Updated

--- nanoesp32blemouse.ino
+++ nanoesp32blemouse.ino
@@ -27,17 +27,21 @@ void loop() {
   int potentiometerValue = analogRead(potentiometerPin);
   int fsrLeftValue2 = analogRead(fsrLeftPin2);
   int fsrRightValue2 = analogRead(fsrRightPin2);
+  int ValueA = -4;
+  int ValueB = 4;
+  int ValueC = 4;
+  int ValueD = -4;
 
   if (potentiometerValue == 1023) {
-    ValueA = -4
-    ValueB = 4
-    ValueC = 4
-    ValueD = -4
+    ValueA = -4;
+    ValueB = 4;
+    ValueC = 4;
+    ValueD = -4;
   } else {
-    ValueA = 4
-    ValueB = -4
-    ValueC = 4
-    ValueD = 4
+    ValueA = 4;
+    ValueB = -4;
+    ValueC = 4;
+    ValueD = 4;
   }
 
   // Read joystick values and move the cursor
@@ -46,14 +50,14 @@ void loop() {
   bleMouse.move(mappedJoystickX, mappedJoystickY, 0);
 
   // Handle left click
-  if ((fsrLeftValue1 > 2000 && potentiometerValue == 1023) || fsrLeftValue2 > 2000) && potentiometerValue == 0) {
+  if (((fsrLeftValue1 > 2000 && potentiometerValue == 1023) || fsrLeftValue2 > 2000) && potentiometerValue == 0) {
       bleMouse.press(MOUSE_LEFT);
     } else {
       bleMouse.release(MOUSE_LEFT);
     }
 
   // Handle right click
-  if ((fsrRightValue1 > 2000 && potentiometerValue == 1023) || fsrRightValue2 > 2000) && potentiometerValue == 0) {
+  if (((fsrRightValue1 > 2000 && potentiometerValue == 1023) || fsrRightValue2 > 2000) && potentiometerValue == 0) {
       bleMouse.press(MOUSE_RIGHT);
     } else {
       bleMouse.release(MOUSE_RIGHT);
